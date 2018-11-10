43bbdc4efe1174a8477268fd764ed726cbb63cae
Iris Cheng <chengiris66@gmail.com>
2018-11-09 21:37:30 -0800

    version 1.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,19 +23,16 @@ var lettersGuessed = "";
 var userInput = ""; // for display purpose
 var randomWord = "";
 
-function checkLetterAlreadyGuessed (char = "", str = "") {
+
+function getNewWord() {
+    return musicInstruments[Math.floor(Math.random() * musicInstruments.length)];
+}
+
+function checkLetterAlreadyGuessed(char = "", str = "") {
     return str.includes(char);
 }
 
-function getNewWord() {
-    return musicInstruments[Math.floor(Math.random() * musicInstruments.length)];   
-}
-
-function updateStats (newWordLength = 0) {
-    for (var i = 0; i < newWordLength; i++) {
-        currentWord += "- "
-    };
-
+function updateStats() {
     document.querySelector("#wins").innerHTML = "Wins: " + winsCounter;
     document.querySelector("#currentWord").innerHTML = "Current Word: " + currentWord;
     document.querySelector("#numofGuessRemaining").innerHTML = "Number of Guesses Remaining: " + numOfGuessRemaining;
@@ -44,15 +41,20 @@ function updateStats (newWordLength = 0) {
 
 // random word computer picked
 randomWord = getNewWord();
-console.log(randomWord);
+console.log(randomWord + " length: " + randomWord.length);
 
-updateStats(randomWord.length);
+for (var i = 0; i < randomWord.length; i++) {
+    currentWord += "-"
+};
+updateStats();
 
-document.onkeyup = function(event) {
+
+
+document.onkeyup = function (event) {
     var keyPressed = event.key;
 
     // check whether the letter is already selected
-    if (checkLetterAlreadyGuessed(keyPressed, randomWord)) {
+    if (checkLetterAlreadyGuessed(keyPressed, lettersGuessed)) {
         // quit the onkeyup() to select another letter
         return;
     };
@@ -61,8 +63,39 @@ document.onkeyup = function(event) {
     lettersGuessed += keyPressed;
     userInput = userInput + " " + keyPressed;
 
+    // update current word
+    var tempStr = "";
+    for (var i = 0; i < randomWord.length; i++) {
 
+        if (currentWord[i] !== "-") {
+            tempStr += currentWord[i];
+        } else if (keyPressed === randomWord[i]) {
+            tempStr += keyPressed;
+        } else {
+            tempStr += "-";
+        };
+    };
+    currentWord = tempStr;
 
+    numOfGuessRemaining--;
+    if (currentWord === randomWord || numOfGuessRemaining === 0) {
+        if (currentWord === randomWord) {
+            winsCounter++;
+        }
+        
+        userInput = "";
+        numOfGuessRemaining = 12;
+        lettersGuessed = "";
+        currentWord = "";
 
-    
+        // random word computer picked
+        randomWord = getNewWord();
+        console.log(randomWord + " length: " + randomWord.length);
+
+        for (var i = 0; i < randomWord.length; i++) {
+            currentWord += "-"
+        };
+    };
+
+    updateStats();
 };
